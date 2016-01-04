fa40163e458bcbfa19a58e2de53bc8996a757c0a
Rhem Soneja <rhemsoneja@yahoo.com>
2016-01-04 23:19:31 +0800

    Added Where's The Barbarian Prototype

---
+++ Projecti/Assets/Scripts/MiniGameScripts/WTB_Gameplay.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WTB_Gameplay : MonoBehaviour 
+{
+	public static WTB_Gameplay instance = null;
+	public GameObject roundOne, roundTwo, roundThree;
+	public Text txtScore, txtRound;
+	public int scoreNum, maxObjects, round;
+	void Start () 
+	{
+		if (instance == null) instance = this;
+
+		scoreNum = 0;
+		maxObjects = 5;
+		round = 1;
+
+		txtScore.text = "SCORE: " + scoreNum + "/" + maxObjects;
+		txtRound.text = "ROUND" + round;
+
+		setRound(1);
+	}
+
+	void Update () 
+	{
+		txtScore.text = "SCORE: " + scoreNum + "/" + maxObjects;
+		txtRound.text = "ROUND " + round;
+	}
+
+	public void setRound(int i)
+	{
+		if(i == 1)
+		{
+			roundOne.SetActive(true);roundTwo.SetActive(false);roundThree.SetActive(false);
+
+		}
+		else if(i==2)
+		{
+			roundOne.SetActive(false);roundTwo.SetActive(true);roundThree.SetActive(false);
+			scoreNum = 0;
+			maxObjects = 8;
+			round = 2;
+		}
+		else if(i==3)
+		{
+			roundOne.SetActive(false);roundTwo.SetActive(false);roundThree.SetActive(true);
+			scoreNum = 0;
+			maxObjects = 10;
+			round = 3;
+		}
+		else
+		{
+			Debug.Log("END!!!");
+		}
+	}
+
+	public void addScore()
+	{
+		scoreNum++;
+	}
+}
---
+++ Projecti/Assets/Scripts/MiniGameScripts/WTB_ObjectClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WTB_ObjectClick : MonoBehaviour 
+{
+	void OnMouseDown()
+	{
+		if(WTB_Gameplay.instance.scoreNum+1 == WTB_Gameplay.instance.maxObjects)
+		{
+			WTB_Gameplay.instance.setRound(WTB_Gameplay.instance.round+1);
+		}
+		else WTB_Gameplay.instance.addScore();
+
+		this.gameObject.SetActive(false);
+	}
+}
